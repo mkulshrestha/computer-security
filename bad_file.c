81f22f056ca162d85ef4ccd88948fecf7ffe722c
mkulshrestha <mayank@Mayanks-MBP.fios-router.home>
2019-02-20 01:28:09 -0500

    first commit

---
+++ bad_file.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+/* 50291406 */
+int main(int argc, char** argv)
+{
+	char buffer[7]; //as specified in the experiment
+	strcpy(buffer, argv[1]);
+	return 0;
+}
